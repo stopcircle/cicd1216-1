d7b4b160f642124c5714e4f7dca91b6208991517
stopcircle <jjy011007@gmail.com>
2025-12-16 10:21:07 +0900

    fix second commit

--- src/main/java/com/example/cicd1216_1/HomeController.java
+++ src/main/java/com/example/cicd1216_1/HomeController.java
@@ -1,10 +1,12 @@
 package com.example.cicd1216_1;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+    @GetMapping("/hi")
     public String hicicd(){
         return "hicicd 1216";
     }
