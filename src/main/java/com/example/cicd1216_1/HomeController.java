7ae8d5f744b285739ac7bf522800b7cd45d24b21
stopcircle <jjy011007@gmail.com>
2025-12-16 11:11:28 +0900

    123

--- src/main/java/com/example/cicd1216_1/HomeController.java
+++ src/main/java/com/example/cicd1216_1/HomeController.java
@@ -18,6 +18,6 @@ public class HomeController {
 
     @GetMapping("/hi3")
     public String hicicd3(){
-        return "새로운 개발 cicd 적용 take123231212";
+        return "새로운 개발 cicd 적용 take123231123212";
     }
 }
