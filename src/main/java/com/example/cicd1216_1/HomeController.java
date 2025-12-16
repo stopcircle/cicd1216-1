b0bb01ba8f51892649318f4339eb78d6d9c97e06
stopcircle <jjy011007@gmail.com>
2025-12-16 10:03:51 +0900

    second commit

---
+++ src/main/java/com/example/cicd1216_1/HomeController.java
@@ -0,0 +1,11 @@
+package com.example.cicd1216_1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    public String hicicd(){
+        return "hicicd 1216";
+    }
+}
